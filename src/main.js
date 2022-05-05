2bb7856824ac192930e4eed9f4e974a475c1a39a
Nikhil Ranjan <niklabh811@gmail.com>
2022-05-05 21:05:58 +0200

    port env

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { harvest, estimate } from './harvester.js';
 const BLOCK_INTERVAL = 12000;
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/api/:network/gasNow', (req, res, next) => {
     const network = req.params.network;
