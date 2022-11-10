e2410c7117f65bfc47d6872b7fa308acfaaaa53c
Nikhil Ranjan <niklabh811@gmail.com>
2022-11-10 11:38:12 +0100

    cors

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@
 // and set up triggers.
 const functions = require('firebase-functions');
 const {harvest} = require('./harvester');
+const cors = require('cors');
 
 const networks = ['astar', 'shiden', 'shibuya'];
 
@@ -9,29 +10,33 @@ const networks = ['astar', 'shiden', 'shibuya'];
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const corsHandler = cors({origin: true});
+
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
 exports.gasNow = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const network = req.query.network;
+  corsHandler(req, res, async () => {
+    // Grab the text parameter.
+    const network = req.query.network;
 
-  if (!network) {
-    return res.json({error: 'network required in query string'});
-  }
+    if (!network) {
+      return res.json({error: 'network required in query string'});
+    }
 
-  const doc = await admin
-      .firestore()
-      .collection('gasfee')
-      .doc(network)
-      .get();
+    const doc = await admin
+        .firestore()
+        .collection('gasfee')
+        .doc(network)
+        .get();
 
-  if (!doc.exists) {
-    return res.json({error: `No data found for ${network} network`});
-  }
+    if (!doc.exists) {
+      return res.json({error: `No data found for ${network} network`});
+    }
 
-  res.json({
-    code: 200,
-    data: doc.data(),
+    res.json({
+      code: 200,
+      data: doc.data(),
+    });
   });
 });
 
